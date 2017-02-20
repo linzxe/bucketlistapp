a2cca9e7f9d18da379ff5844f2faea02dd1c6131
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-20 13:22:35 -0500

    route handlers

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ var express = require("express");
 var http = require("http");
 var bodyParser = require("body-parser");
 var app = express();
+var router = require("./router");
 
+//middleware
 app.use(bodyParser.json({type: '*/*'}));
+router(app);
 
+//server
 var port = process.env.PORT || 3000;
-
 var server = http.createServer(app);
 
 server.listen(port);
