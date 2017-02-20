15ac2f562c6f77c321ff4045b7dbcf3b37275bb0
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-20 16:30:32 -0500

    routine commit

--- server/router.js
+++ server/router.js
@@ -1,8 +1,16 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){
-	
+
+	app.get('/', requireAuth, function(req, res){
+		res.send('Hello, Homepage');
+		//res.send({hi:'there'});
+	});
+
 	app.post('/signup', Auth.signup);
 }
 
