3e56499832a0493c624c654b330a27b42787a69e
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-24 11:46:03 -0500

    added signin form

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import NavBarHeader from './Nav';
-import Video from './video/video'
+import Video from './video/video';
+import Signin from './auth/signin';
 	
 	export default class App extends Component {
 		render() {
 			return (
 				<div>
-				<NavBarHeader />
-				<Video />
+					<NavBarHeader />
+					<Video />
+					{this.props.children}
 				</div>
 				);
 		}
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import Signin from './components/auth/signin';
 
 var createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -12,6 +13,7 @@ ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
+				<Route path="signin" component={Signin} />
 			</Route>
 		</Router>
 	</Provider>
