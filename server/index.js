de006efc1c0fb88ff15137460a926669dd08c395
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-24 17:35:21 -0500

    routine commit

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ var bodyParser = require("body-parser");
 var app = express();
 var router = require("./router");
 var mongoose = require("mongoose");
+var cors = require("cors");
 
 //db connection
 mongoose.connect("mongodb://localhost:bucket/bucket");
 
+app.use(cors());
 //middleware
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
