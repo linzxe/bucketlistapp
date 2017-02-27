5620a9f7448bbc4e59928f826c014f9fc041e426
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-27 08:48:39 -0500

    routine commit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 // export const CREATE_POSTS = 'CREATE_POSTS';
 
@@ -6,15 +7,21 @@ import axios from 'axios';
 const ROOT_URL = 'http://localhost:3000';
 export const CREATE_POSTS = 'CREATE_POSTS';
 
-export function createPost(props) {
-	const request = axios.post(`${ROOT_URL}/posts`, props);
-	return {
-		type: CREATE_POSTS,
-		payload: request
-	};
-}
+// export function createPost(props) {
+// 	const request = axios.post(`${ROOT_URL}/posts`, props);
+// 	return {
+// 		type: CREATE_POSTS,
+// 		payload: request
+// 	};
+// }
 export function signinUser({ email, password }) {
 	return function(dispatch){
 		axios.post(`${ROOT_URL}/signin`, {email, password})
+		.then(response => {
+			browserHistory.push('/newitem');
+		})
+			.catch(() => {
+
+			});
 	}
 }
