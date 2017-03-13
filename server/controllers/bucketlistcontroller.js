97dff721e282a0a621026b18ac25696687df5a1a
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-03-13 16:52:55 -0400

    changes made

---
+++ server/controllers/bucketlistcontroller.js
@@ -0,0 +1,24 @@
+var BucketList = require('../models/bucketlist.js');
+
+exports.addBucketList = function(req, res, next){
+	//for postman use
+	//var title = req.body.title;
+	var title = req.body.props.title;
+	var topic = req.body.props.topic;
+	var url = req.body.props.url;
+	var content = req.body.props.content;
+	var specificUser = req.user;
+
+	var bucketList = new BucketList({
+		title: title,
+		topic: topic,
+		url: url,
+		content: content,
+		specificUser: specificUser
+	});
+
+	bucketList.save(function(err){
+		if(err) { return next(err); }
+		res.json(bucketList);
+	});
+}
--- server/router.js
+++ server/router.js
@@ -1,17 +1,20 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var BucketList = require('./controllers/bucketlistcontroller');
+
 var passportService = require('./services/passport');
 var passport = require('passport');
+
 var requireAuth = passport.authenticate('jwt', {session: false});
 var requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
 
-	app.get('/', requireAuth, function(req, res){
-		res.send({message: 'hey'});
-		//res.send({hi:'there'});
-	});
+	// app.get('/', requireAuth, function(req, res){
+	// 	res.send({message: 'hey'});
+	// 	//res.send({hi:'there'});
+	// });
 
 	app.post('/signup', Auth.signup);
 	app.post('/signin', requireSignin, Auth.signin);
+	app.post('/newitem', requireAuth, BucketList.addBucketList);
 }
